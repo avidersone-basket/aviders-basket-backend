c0b3906cada41df2313e4eff18263ffd515c3311
avidersone-basket <avidersone@gmail.com>
2025-12-19 15:08:01 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
-import basketCategories from "./routes/basketCategories.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
 await connectDB();
-app.use(basketCategories);
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Basket server running on port ${PORT}`);
   console.log(`🌐 API endpoint: http://localhost:${PORT}/basket`);
